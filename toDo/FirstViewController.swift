0cbc10aa7d6ab3e6b41a37be055d0ad22c8cd534
Giovanni Rosas-Marin <geo6240@gmail.com>
2019-05-20 21:27:26 -0700

    to do list app

--- toDo/FirstViewController.swift
+++ toDo/FirstViewController.swift
@@ -7,9 +7,64 @@
 //
 
 import UIKit
+ var list = ["Buy soda", " Run to School", "Get food", "Study"]
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+           list.remove(at: indexPath.row)
+            myTableView.reloadData()
+            
+        }
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if myTableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark {
+            myTableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+        else{
+            myTableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+        
+    }
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+//    {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//
+//        if indexPath.row < list.count
+//        {
+//            let item = todoItems[indexPath.row]
+//            item.done = !item.done
+//
+//            tableView.reloadRows(at: [indexPath], with: .automatic)
+//        }
+//    }
+//
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+   
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- toDo/SecondViewController.swift
+++ toDo/SecondViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var input: UITextField!
+    
+    
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func addTask(_ sender: Any) {
+        
+        if(input.text != ""){
+        
+        list.append(input.text!)
+            input.text = ""
+    }
 }
 
+}
